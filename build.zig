6e33b9494cac972219d23ff661e5f03aab61c5b6
Ramon Asuncion <ramon@b1t.dev>
2024-11-23 01:18:14 -0500

    Rework build system.

--- build.zig
+++ build.zig
@@ -7,11 +7,5 @@ pub fn build(b: *std.Build) void {
         .target = b.host,
     });
 
-    const run_exe = b.addRunArtifact(exe);
-    if (b.args) |args| {
-        run_exe.addArgs(args);
-    }
-
-    const run_step = b.step("run", "Run filecount.");
-    run_step.dependOn(&run_exe.step);
+    b.installArtifact(exe);
 }
